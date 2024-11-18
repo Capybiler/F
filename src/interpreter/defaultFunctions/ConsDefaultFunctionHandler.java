dca5e18ac1cc25e951064a7c7aa0ada2bf59ad5a
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-11-18 23:59:03 +0300

    Implement cons

--- src/interpreter/defaultFunctions/ConsDefaultFunctionHandler.java
+++ src/interpreter/defaultFunctions/ConsDefaultFunctionHandler.java
@@ -1,5 +1,9 @@
 package interpreter.defaultFunctions;
 
+import parser.nodes.ASTNode;
+import parser.nodes.ListASTNode;
+import parser.nodes.LiteralASTNode;
+
 import java.util.List;
 
 public class ConsDefaultFunctionHandler extends DefaultFunctionHandler {
@@ -10,7 +14,22 @@ public class ConsDefaultFunctionHandler extends DefaultFunctionHandler {
 
     @Override
     public Object handle() {
-        // TODO: Implement this
-        return null;
+        if (!(parameters.get(1) instanceof ListASTNode)) {
+            throw new IllegalArgumentException("Expected list as a parameter, but got " + parameters.get(1).getClass().getSimpleName());
+        }
+
+        ASTNode node;
+
+        if (parameters.getFirst() instanceof ASTNode) {
+            node = (ASTNode) parameters.getFirst();
+        } else {
+            node = new LiteralASTNode(parameters.getFirst());
+        }
+
+        ListASTNode list = (ListASTNode) parameters.get(1);
+
+        list.getElements().addFirst(node);
+
+        return list;
     }
 }
