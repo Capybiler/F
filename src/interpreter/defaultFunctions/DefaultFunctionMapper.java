902d3b3a033838a2c7777023caded63ae89d302b
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-11-18 19:14:49 +0300

    Add functions to mapper

--- src/interpreter/defaultFunctions/DefaultFunctionMapper.java
+++ src/interpreter/defaultFunctions/DefaultFunctionMapper.java
@@ -7,8 +7,34 @@ public class DefaultFunctionMapper {
         return switch (functionName) {
             case "plus" -> new PlusDefaultFunctionHandler(parameters);
             case "minus" -> new MinusDefaultFunctionHandler(parameters);
-            case "divide" -> new DivideDefaultFunctionHandler(parameters);
             case "times" -> new MultiplyDefaultFunctionHandler(parameters);
+            case "divide" -> new DivideDefaultFunctionHandler(parameters);
+
+            case "head" -> new HeadDefaultFunctionHandler(parameters);
+            case "tail" -> new TailDefaultFunctionHandler(parameters);
+            case "cons" -> new ConsDefaultFunctionHandler(parameters);
+
+            case "equal" -> new EqualDefaultFunctionHandler(parameters);
+            case "nonequal" -> new NonEqualDefaultFunctionHandler(parameters);
+            case "less" -> new LessDefaultFunctionHandler(parameters);
+            case "lessEq" -> new LessEqDefaultFunctionHandler(parameters);
+            case "greater" -> new GreaterDefaultFunctionHandler(parameters);
+            case "greaterEq" -> new GreaterEqDefaultFunctionHandler(parameters);
+
+            case "isint" -> new IsIntDefaultFunctionHandler(parameters);
+            case "isreal" -> new IsRealDefaultFunctionHandler(parameters);
+            case "isbool" -> new IsBoolDefaultFunctionHandler(parameters);
+            case "isnull" -> new IsNullDefaultFunctionHandler(parameters);
+            case "isatom" -> new IsAtomDefaultFunctionHandler(parameters);
+            case "islist" -> new IsListDefaultFunctionHandler(parameters);
+
+            case "and" -> new AndDefaultFunctionHandler(parameters);
+            case "or" -> new OrDefaultFunctionHandler(parameters);
+            case "xor" -> new XorDefaultFunctionHandler(parameters);
+            case "not" -> new NotDefaultFunctionHandler(parameters);
+
+            case "eval" -> new EvalDefaultFunctionHandler(parameters);
+
             default -> null;
         };
     }
