567af1ebf24ce9d9f8c2a9e7e8b98bd425929975
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-11-18 23:10:42 +0300

    Implement IsIntDefaultFunctionHandler

--- src/interpreter/defaultFunctions/IsIntDefaultFunctionHandler.java
+++ src/interpreter/defaultFunctions/IsIntDefaultFunctionHandler.java
@@ -10,7 +10,6 @@ public class IsIntDefaultFunctionHandler extends DefaultFunctionHandler {
 
     @Override
     public Object handle() {
-        // TODO: Implement this
-        return null;
+        return this.parameters.getFirst() instanceof Integer;
     }
 }
