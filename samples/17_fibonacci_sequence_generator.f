8f49459c2c672ff9588b97df5b082b17fb37456e
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-11-18 22:07:29 +0300

    Fix sample

--- samples/17_fibonacci_sequence_generator.f
+++ samples/17_fibonacci_sequence_generator.f
@@ -1,6 +1,11 @@
 (func fib (n)
-  (cond (lesseq n 0) 0
-        (equal n 1) 1
-        true (plus (fib (minus n 1)) (fib (minus n 2))))
+    (cond (lesseq n 0)
+        0
+        (cond (equal n 1)
+            1
+            (plus (fib (minus n 1)) (fib (minus n 2)))
+        )
+    )
 )
+
 (fib 10)
