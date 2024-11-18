a0b170b9524d61783f3a745a4445158890672d9e
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-11-18 04:20:40 +0300

    Improve DefaultFunctionHandler

--- src/interpreter/defaultFunctions/DefaultFunctionHandler.java
+++ src/interpreter/defaultFunctions/DefaultFunctionHandler.java
@@ -4,10 +4,22 @@ import java.util.List;
 
 public abstract class DefaultFunctionHandler {
     protected final List<Object> parameters;
+    protected int expectedParametersCount;
 
     DefaultFunctionHandler(List<Object> parameters) {
         this.parameters = parameters;
     }
 
+     public Object handleWithCommonChecks() {
+        checkParametersCount(expectedParametersCount);
+        return handle();
+    }
+
     public abstract Object handle();
+
+    protected void checkParametersCount(int expectedCount) {
+        if (parameters.size() != expectedCount) {
+            throw new IllegalArgumentException("Expected " + expectedCount + " parameters, but got " + parameters.size());
+        }
+    }
 }
