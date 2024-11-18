2cc9d0f6243a96cf15758b87b08832fa0ffcd623
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-11-18 19:10:52 +0300

    Implement equal, nonequal

--- src/interpreter/defaultFunctions/DefaultFunctionHandler.java
+++ src/interpreter/defaultFunctions/DefaultFunctionHandler.java
@@ -10,12 +10,21 @@ public abstract class DefaultFunctionHandler {
         this.parameters = parameters;
     }
 
-     public Object handleWithCommonChecks() {
+    public Object handleWithCommonChecks() {
         checkParametersCount(expectedParametersCount);
         return handle();
     }
 
     public abstract Object handle();
+
+    protected double convertBoolIntDoubleToDouble(Object value, String errorMessage) {
+        return switch (value) {
+            case Boolean b -> (boolean) value ? 1 : 0;
+            case Integer i -> (double) (int) value;
+            case Double v -> (double) value;
+            case null, default -> throw new IllegalArgumentException(errorMessage);
+        };
+    }
 
     protected void checkParametersCount(int expectedCount) {
         if (parameters.size() != expectedCount) {
